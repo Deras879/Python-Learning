5dbc8ce594185ab5fc707d5ac1f01330f772b15c
deras879 <derycard59@gmail.com>
2025-08-04 02:49:17 -0500

    Added new lessons

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  build: {
+    outDir: "dist",
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
   },
 });
